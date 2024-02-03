51c7432876554650af1d5495d83cf06463b85d2d
Mohadese Zamani <mohizamani2001@gmail.com>
2024-02-03 18:10:21 +0330

    logic layer added

---
+++ Logic_Layer/GroupLogic.py
@@ -0,0 +1,24 @@
+from .Group import Group
+from ..Data_Layer import DataAccess
+
+class ChatLogicHandler:
+    config=" "
+
+    def Create_Group(group_name,group_admin_id):
+        return "Not Implemented"
+    
+    def Add_Member(username):
+        return "Not Implemented"
+    
+    def Remove_Member(username):
+        return "Not Implemented"
+    
+    def Edit_Group(group_profile,group_name,group_admin_id):
+        return "Not Implemented"
+    
+    def Get_Group_info():
+        return "Not Implemented"
+    
+    
+    
+    
