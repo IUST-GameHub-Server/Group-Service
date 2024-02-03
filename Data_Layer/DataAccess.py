bf4ac0dfb7c46f033cf2222dd5e43252bae0d55d
Mohadese Zamani <mohizamani2001@gmail.com>
2024-02-03 17:35:40 +0330

    data access added

---
+++ Data_Layer/DataAccess.py
@@ -0,0 +1,17 @@
+class Data_Access:
+    database_address=''
+   
+    def Create_Group(group_name,group_image):
+        return "Not implimented"
+    
+    def Set_Group_Admin(username):
+        return "Not implimented"
+    
+    def Get_Group_Members(group_name):
+        return "Not implimented"
+    
+    def Get_Group_Admins(group_name):
+        return "Not implimented"
+    
+    def Get_History_Messages():
+        return "Not implimented"
