ff6b9228fd7e4a85ac15152f2b01129174533b56
MohadeseZamani <157816618+MohadeseZamani@users.noreply.github.com>
2024-02-05 02:50:02 +0330

    Update DataAccess.py

--- Data_Layer/DataAccess.py
+++ Data_Layer/DataAccess.py
@@ -1,12 +1,15 @@
 class Data_Access:
     database_address=''
-   
-    def Create_Group(group_name,group_image):
-        return "Not implimented"
     
     def Set_Group_Admin(username):
         return "Not implimented"
-    
+
+    def Set_Group_Name(group_name):
+        return "Not implimented"
+
+     def Set_Group_Image(group_image):
+        return "Not implimented"
+
     def Get_Group_Members(group_name):
         return "Not implimented"
     
